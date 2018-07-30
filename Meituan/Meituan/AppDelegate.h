ba0dd551ffb93990799ed9b6ca6fcd4a58dfa21a
RC <RC@163.com>
2018-07-30 11:35:55 +0800

    初始化项目

---
+++ Meituan/Meituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Meituan
+//
+//  Created by Dean_ on 2018/7/30.
+//  Copyright © 2018年 Dean_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Meituan/Meituan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Meituan
+//
+//  Created by Dean_ on 2018/7/30.
+//  Copyright © 2018年 Dean_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
